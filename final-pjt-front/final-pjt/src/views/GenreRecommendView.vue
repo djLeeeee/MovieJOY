a9290b5b8189848d2e58d27575a4abe86a2e8d8d
Hyelim <hallov012@gmail.com>
2022-05-23 02:44:51 +0900

    create genre btns

--- final-pjt-front/final-pjt/src/views/GenreRecommendView.vue
+++ final-pjt-front/final-pjt/src/views/GenreRecommendView.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
+  <div id="genre-reccomend-box">
     This is a movie recommendation by genre page.
-    <div v-for="(data, idx) in moviesByGenre" :key="idx">
-      <button @click="selectGenre(idx)">{{ data['genre_name'] }}</button>
+    <div id="genre-box" class="row row-cols-5 row-cols-md-6 row-cols-lg-8 row-cols-xl-10 g-1">
+      <div v-for="(data, idx) in moviesByGenre" :key="idx">
+        <button @click="selectGenre(idx)" class="col custom-btn btn-12 bubbly-button">
+          <span>Click!</span>
+          <span>{{ data['genre_name'] }}</span>
+        </button>
+      </div>
+    </div>
+    <div id="genre-movie-box">
+      <MovieList :movies="selectedMovies"/>
     </div>
     <p>{{ selectedGenreName }}</p>
-    <MovieList :movies="selectedMovies"/>
+    
   </div>
 </template>
 
@@ -95,6 +103,10 @@
             'id': 28,
             'name': '액션',
           },
+          {
+            'id': 35,
+            'name': '코미디',
+          }
         ],
         moviesByGenre: [],
         selectedMovies: [],
@@ -106,6 +118,8 @@
     },
     methods: {
       selectGenre: function (idx) {
+
+
         this.selectedMovies = this.moviesByGenre[idx].movies
         this.selectedGenreName = this.moviesByGenre[idx].genre_name
       }
@@ -130,5 +144,86 @@
 </script>
 
 <style>
+#genre-reccomend-box {
+  padding: 2rem;
+}
 
+#genre-box {
+  display: flex;
+}
+
+.custom-btn {
+  width: 130px;
+  height: 40px;
+  color: #fff;
+  border-radius: 5px;
+  padding: 10px 25px;
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  background: transparent;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  outline: none;
+}
+
+.btn-12{
+  position: relative;
+  right: 20px;
+  bottom: 20px;
+  border:none;
+  box-shadow: none;
+  width: 130px;
+  height: 40px;
+  line-height: 42px;
+  -webkit-perspective: 230px;
+  perspective: 230px;
+}
+.btn-12 span {
+  display: block;
+  position: absolute;
+  width: 130px;
+  height: 40px;
+  border-radius: 5px;
+  margin:0;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+
+.btn-12 span:nth-child(1) {
+  -webkit-transform: rotateX(90deg);
+  -moz-transform: rotateX(90deg);
+  transform: rotateX(90deg);
+  -webkit-transform-origin: 50% 50% -20px;
+  -moz-transform-origin: 50% 50% -20px;
+  transform-origin: 50% 50% -20px;
+  color: #00d7e2ec;
+}
+
+.btn-12 span:nth-child(2) {
+  -webkit-transform: rotateX(0deg);
+  -moz-transform: rotateX(0deg);
+  transform: rotateX(0deg);
+  -webkit-transform-origin: 50% 50% -20px;
+  -moz-transform-origin: 50% 50% -20px;
+  transform-origin: 50% 50% -20px;
+}
+
+.btn-12:hover span:nth-child(1) {
+  -webkit-transform: rotateX(0deg);
+  -moz-transform: rotateX(0deg);
+  transform: rotateX(0deg);
+}
+
+.btn-12:hover span:nth-child(2) {
+ color: transparent;
+  -webkit-transform: rotateX(-90deg);
+  -moz-transform: rotateX(-90deg);
+  transform: rotateX(-90deg);
+}
 </style>
