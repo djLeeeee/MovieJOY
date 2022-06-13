c119c8b4bfb86cbd69535af35001c6c964f5f084
Hyelim <hallov012@gmail.com>
2022-06-13 22:04:19 +0900

    secret-key

--- final-pjt-back/pjt/settings.py
+++ final-pjt-back/pjt/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(p7p0#$^FHHdfsFGHHdggthefdfsrerg04ververvewkjbnp3290vdvfd)w')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(g$ua*_j5-q@m%kv&$8t+1k8e(h)=k9pq7)9&jc8(dc2^9$%ui')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
