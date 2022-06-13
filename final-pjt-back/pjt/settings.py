a3f85f9ecf97b061d0fc87e5ecc984496a8f7f74
이동준 <bomul1128@naver.com>
2022-06-13 23:24:39 +0900

    BackEnd Deploy Complete

--- final-pjt-back/pjt/settings.py
+++ final-pjt-back/pjt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(g$ua*_j5-q@m%kv&$8t+1k8e(h)=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
